65c6c0de460785647ac783fbf2cf15bf935f4469
minoli <minoliperera91@gmail.com>
2019-05-12 19:55:41 +0530

    Add portal app

---
+++ components/portal/src/components/appLayout/index.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Container from '@material-ui/core/Container';
+import Footer from "./footer";
+import Header from "./header";
+import React from "react";
+import {withStyles} from "@material-ui/core/styles";
+import * as PropTypes from "prop-types";
+
+const styles = () => ({
+    root: {
+        display: 'flex',
+        minHeight: '100vh',
+        flexDirection: 'column'
+    },
+    mainContent: {
+        flex: 1
+    },
+});
+
+class AppLayout extends React.Component {
+
+    render = () => {
+        const {classes, children} = this.props;
+
+        return (
+            <div className={classes.root}>
+                <Header/>
+                <main className={classes.mainContent}>
+                    <Container maxWidth="md">
+                        {children}
+                    </Container>
+                </main>
+                <Footer/>
+            </div>
+        );
+    };
+
+}
+
+AppLayout.propTypes = {
+    classes: PropTypes.object.isRequired,
+    children: PropTypes.any.isRequired,
+};
+
+export default withStyles(styles)(AppLayout);
+
---
+++ components/portal/src/components/explore/index.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Divider from '@material-ui/core/Divider';
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from "@material-ui/core/styles";
+import * as PropTypes from "prop-types";
+
+const styles = (theme) => ({
+    content: {
+        paddingTop: theme.spacing.unit * 4
+    }
+});
+
+class Explore extends React.Component {
+
+    render = () => {
+        const {classes} = this.props;
+
+        return (
+            <div className={classes.content}>
+                <Typography variant="h5" color="inherit">
+                    Explore
+                </Typography>
+                <Divider/>
+                //TODO: Add explore page content
+            </div>
+        );
+    };
+
+}
+
+Explore.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Explore);
